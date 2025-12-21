be92973b309129ef22a1a5aca41ddb6a308a5763
Gaurav Purohit <gaurav.purohit@celebaltech.com>
2025-12-21 19:49:55 +0530

    added the feature of streaming

--- frontend.py
+++ frontend.py
@@ -21,7 +21,7 @@ CONFIG={
     }
 }
 
-if 'message-history' not in st.session_state:
+if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
 for message in st.session_state['message_history']:
@@ -38,13 +38,15 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = chatbot.invoke(
-        {'messages': [HumanMessage(content=user_input)]},
-        config=CONFIG
-    )
-
-    ai_message = response['messages'][-1].content
+    with st.chat_message('assistant'):
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {
+                    'messages': [HumanMessage(content=user_input)]
+                },
+                config=CONFIG,
+                stream_mode='messages'
+            )
+        )
 
     st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
-    with st.chat_message('assistant'):
-        st.text(ai_message)
