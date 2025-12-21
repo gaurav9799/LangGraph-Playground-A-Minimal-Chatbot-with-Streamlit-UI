807563a01efba64332d7bf2442b6dc9257bb10df
Gaurav Purohit <gaurav.purohit@celebaltech.com>
2025-12-21 17:19:35 +0530

    added streamlit frontent and LangGraph backend to develop a minimal chatbot which remebers our conversetional history

---
+++ frontend.py
@@ -0,0 +1,50 @@
+"""
+Streamlit UI for interacting with a LangGraph-based chatbot.
+
+This module provides a minimal chat interface using Streamlit that:
+- Captures user input via Streamlit's chat components
+- Maintains conversation history in Streamlit session state
+- Sends user messages to a LangGraph-powered chatbot backend
+- Renders assistant responses in a conversational format
+
+The UI is intentionally lightweight and designed for rapid
+experimentation and local development rather than production deployment
+"""
+
+import streamlit as st
+from langchain_core.messages import HumanMessage
+from langgraph_backend import chatbot
+
+CONFIG={
+    'configurable': {
+        'thread_id': 'thread-1'
+    }
+}
+
+if 'message-history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    st.session_state['message_history'].append(
+        {'role': 'user', 'content': user_input}
+    )
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    response = chatbot.invoke(
+        {'messages': [HumanMessage(content=user_input)]},
+        config=CONFIG
+    )
+
+    ai_message = response['messages'][-1].content
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    with st.chat_message('assistant'):
+        st.text(ai_message)
