452ef5114fea3541202d0154539196f755397b11
Gaurav Purohit <gaurav.purohit@celebaltech.com>
2025-12-23 13:34:02 +0530

    Integrated sqlite database to save our all chats, Removed InMemorySaver

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -14,11 +14,12 @@ and experimentation rather than production use.
 import os
 from typing import TypedDict, Annotated
 from langgraph.graph import StateGraph, START, END
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from langchain_core.messages import BaseMessage
 from langchain_openai import AzureChatOpenAI
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
@@ -70,7 +71,12 @@ def chat_node(state: ChatState):
     response = llm.invoke(messages)
     return {"messages": [response]}
 
-checkpointer = InMemorySaver()
+conn=sqlite3.connect(
+    database='chatbot.db',
+    check_same_thread=False
+)
+
+checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
@@ -78,3 +84,10 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads=set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
